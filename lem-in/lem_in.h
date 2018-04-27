e67f780aae82b8c57e6f15e9105fe9914c3573c0
Alexandre COTT <alecott@e3r5p22.42.fr>
2018-04-27 17:04:23 +0200

    I see the light

--- lem-in/lem_in.h
+++ lem-in/lem_in.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lem-in.h                                           :+:      :+:    :+:   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alecott <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/01 10:13:35 by alecott           #+#    #+#             */
-/*   Updated: 2018/04/25 17:08:32 by alecott          ###   ########.fr       */
+/*   Updated: 2018/04/27 13:41:32 by alecott          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,21 @@
 typedef struct	s_ants
 {
 	char	**rooms;
-	int		*rooms_state;
 	char	**tubes;
 	char	**tubes_names;
 	char	**names;
 	char	*start;
 	char	*end;
+	char	*room;
+	char	*rm;
+	char	*path;
+	int		nb_repeat;
+	int		l;
+	int		k;
+	int		j;
+	int		m;
 	int		nb_room;
+	int		nb_tubes;
 	int		nb_ant;
 	char	**room_ant;
 	char	**path_ant;
@@ -34,18 +42,33 @@ typedef struct	s_ants
 void	ft_lem_in(t_ants *info);
 int		ft_stock(t_ants *info);
 void	ft_names(t_ants *info);
-void	ft_BFS(t_ants *info);
 int		ft_error(t_ants *info);
-char	*ft_find_dawae(t_ants *info, int ants);
 char	*ft_link(char *room, char *tube, t_ants *info);
-char	*ft_where_is_my_ant(t_ants *info, int ants);
 int		ft_room_number(t_ants *info, char *room);
 int		ft_norm(t_ants *info, int *i, int *j, int *n);
 int		ft_check_ant(t_ants *info, char *room);
 int		ft_tubestr(char *s1, char *s2);
 int		ft_verify(char *look, char *end, t_ants *info);
-void	ft_fill_path(t_ants info);
+void	ft_fill_path(t_ants *info);
+void	ft_move_tubes(char **tubes);
+void	ft_feel_free(t_ants *info, char **clonepath);
+char	*ft_rm_last_one(char *path);
+void	ft_sort_tubes_and_repeat(t_ants *info, int *i);
+char	*ft_get_room(char *room, char *link);
+int		ft_find_room_intube(char *room, char *link);
+char	*ft_get_last_inpath(char *path);
+char	*ft_complete_path(char *path, char *room, int i, t_ants info);
+int   ft_check_ifexist(char *room, char *path);
+void	ft_add_and_delete(t_ants *info, char **allpath);
+int		ft_check_path(char *path, char **allpath);
+void	ft_find_nbroom(t_ants *info);
+char	*ft_remove_ifrepeat(char *rm, int *m,  char *path, char *room);
+void	ft_choose_nbofrm(int *clonem, t_ants *info);
+void	ft_find_nb_tubes(t_ants *info);
 void	ft_algo(t_ants *info, char **path);
 void	ft_tubes_names(t_ants *info);
+int		ft_nbrooms_in_path(char *path);
+char	**ft_sort_paths(char **path);
+char	*ft_sub_path(char *path);
 
 #endif
